dafb4cc05e4c4bd9cc5b8cb6cffc84e4fed32130
Galen O'Shea-Stone, PhD <54643069+Galenosheastone@users.noreply.github.com>
2025-03-04 11:54:46 -0700

    Update 1_Streamlit_NMR_Processing_v2.1.py

--- Copie_Group_Streamlit_Stats_Tools/pages/1_Streamlit_NMR_Processing_v2.1.py
+++ Copie_Group_Streamlit_Stats_Tools/pages/1_Streamlit_NMR_Processing_v2.1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Streamlit_NMR_Processing_v2.0.py", layout="wide")
+st.set_page_config(page_title="NMR_Processing_v2.0.py", layout="wide")
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
